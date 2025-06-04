d4d16e2c017a52cc29ce0241ba287df7ca338dcd
Kishore Kaseebhotla <kaseebhotla@hotmail.com>
2025-06-04 09:07:45 -0700

    trying to fix the 404 error

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Hello from clean Next.js</h1>
+      <h1>Hello from clean Next.js!</h1>
     </div>
   );
 }
