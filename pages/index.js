e41c84e6f50289e9f26f9fdb0c16b750f5471ad3
Kishore Kaseebhotla <kaseebhotla@hotmail.com>
2025-06-04 08:59:36 -0700

    trimming the fat...

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,7 @@
 export default function Home() {
   return (
     <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Hello from Next.js!</h1>
-      <p>If you're seeing this, your routing is working 🎉</p>
+      <h1>Hello from clean Next.js</h1>
     </div>
-  )
+  );
 }
